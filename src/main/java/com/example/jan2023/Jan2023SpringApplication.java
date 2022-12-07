e2661525dfd0b6eb253e95925f16c01f5cfbecda
Namratha Kondi <namratha.9966@gmail.com>
2022-12-07 16:31:02 -0500

    First Commit

---
+++ src/main/java/com/example/jan2023/Jan2023SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.jan2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jan2023SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jan2023SpringApplication.class, args);
+	}
+
+}
